37e469b393b2d27712e76d4b0fe20e58f8a6de6d
Mykhailo Panasenko <mpanasenko@luxoft.com>
2018-07-24 11:44:40 +0300

    cleanning scripts and add comments

--- king_slave.sh
+++ king_slave.sh
@@ -23,14 +23,19 @@ rm -rf $BR_LOC/$BARE_REPO_SLAVE_2
 	### CLONE INITIALIZATION ###
 	cd $WD_LOC
 		git clone $BR_LOC/$BARE_REPO_KING
-                git clone $BR_LOC/$BARE_REPO_SLAVE_1
+		git clone $BR_LOC/$BARE_REPO_SLAVE_1
+		git clone $BR_LOC/$BARE_REPO_SLAVE_2
+		### CLONE SETUP ###
+		cd $WD_LOC/$CLONE_KING
+			echo "Initial commit king" >> readme.txt
+                	git add readme.txt
+                	git commit -m "Initial commit king"
+                	git push origin master
                 cd $WD_LOC/$CLONE_SLAVE1
                 	echo "Initial commit slave1" >> readme.txt
                 	git add readme.txt
                 	git commit -m "Initial commit slave1"
                 	git push origin master
-	cd $WD_LOC
-		git clone $BR_LOC/$BARE_REPO_SLAVE_2
                 cd $WD_LOC/$CLONE_SLAVE2
                         echo "Initial commit slave2" >> readme.txt
                         git add readme.txt
@@ -38,32 +43,28 @@ rm -rf $BR_LOC/$BARE_REPO_SLAVE_2
                         git push origin master
 	### KING SETUP ###
 	cd $WD_LOC/$CLONE_KING
-		echo "Initial commit" >> readme.txt
-		git add readme.txt
-		git commit -m "Initial commit"
-		git push origin master
 		git remote add slave1 $BR_LOC/$BARE_REPO_SLAVE_1
 		git remote add slave2 $BR_LOC/$BARE_REPO_SLAVE_2
-			### Update remote changes ###
+		### Update remote changes ###
 		git fetch slave1
 		git fetch slave2
-			### Creating local copy of branch slave1 ###
+		### Creating local copy of branch slave1 ###
 		git checkout -b slave1_master slave1/master
 		git branch -a -vv
-			### creating new commit on slave1_master branch ###
+		### creating new commit on slave1_master branch ###
 		echo "commit Slave1" >> slave1.txt
 		git add slave1.txt
                 git commit -m "commit slave1"
                 git push slave1 slave1_master:master
-			### Creating local copy of branch slave2 ###
+		### Creating local copy of branch slave2 ###
 		git checkout -b slave2_master slave2/master
                 git branch -a -vv
-                        ### creating new commit on slave1_master branch ###
+                ### creating new commit on slave2_master branch ###
                 echo "commit Slave2" >> slave2.txt
                 git add slave2.txt
                 git commit -m "commit slave2"
                 git push slave2 slave2_master:master
-
+		### Returning HEAD to master branch ###
 		git checkout master
 
 	### CHECKING RESULT ###
